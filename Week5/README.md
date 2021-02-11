39566d879850c87ec5c169646a5d540e93114473
Debnath <debnsuma@3c22fb299a33.ant.amazon.com>
2021-02-12 00:29:00 +0530

    Adding Week5 Assignment 2

--- Week5/README.md
+++ Week5/README.md
@@ -146,3 +146,77 @@ def test_function_score_cal_check25():
 def test_function_score_cal_check26():
     assert score_calculator(p_high_card_3)[0]==1000, 'score Should be 1000'
 
+
+For Problem 2
+==============
+
+Here we have various small tasks as mentioned above, so lets take them one at a time
+
+### Tasks
+
+#### **Task 1**
+
+In this task we are asked to check whether a number falls in the fabonacci series or not using **list**, **filters** and **lambda functions**. Here we create a function Fibonacci which returns the fabonacci series, Then randomly a list of numbers is generated and matched against the fabonacci list and the matched list is displayed.
+
+#### **Task 2**
+
+All the sub tasks here has to be done using list comprehension. 
+   * A We need to take two list and check each elements, if element of list a is even where as element of list b is odd, we will add them else we will ignore them
+     * User sends two list
+     * The list is zipped and iterated
+     * First tuple element is checked for evenness and second tuple element is checked for oddness
+     * if the condition is matched those elements are added
+   * We have to remove vowels from the string
+     * iterate through string
+     * check if element falls in the vowel category
+   * Make a Relu Function
+     * Iterate over the list of numbers
+     * if Negative convert to zero, 
+     * else leave as it is
+   * Make a sigmoid function
+     * Create a sigmoid function that takes integer as an input and returns its sigmoid form
+     * iterate through the list that user provided and pass each element to sigmoid function
+     * Store each element to a new list and return the list
+   * Make an Alphabet Cipher
+     * check order of each character
+     * if order + 5 is greater than order of z which is 122, 
+       * then substract 26 from the order of character +5 
+     * else add 5 to the order of character
+     * join the list to make string
+     * return string
+
+#### **Task 3**
+
+In this function the user inputs the paragraph of length 200, if the paragragh word count is less than 200, it raises an error. There is predefined list of bad words [link](https://github.com/RobertJGabriel/Google-profanity-words/blob/master/list.txt ), it is fetched and converted to list. Then the list is check with each word in the para, of any word is found it is alerted.
+
+#### **Task 4**
+
+Here we need to perform tasks using reduce function
+
+*   Add Only even numbers in the list
+    *   filter is applied over the list to fetch the even numbers
+    *   all the numbers are added using reduce function and lambda function performing the addition
+*   To find the biggest character
+    *   Here Max function is used
+    *   Initial value is " " as this is the first ascii printable character
+    *   the input string as passed as argument and max character is returned
+*   Add Every 3 number in the list
+    *   filter list to get every 3rd element i.e. at index 2,5,8,..
+    *   use lambda function to perform addition over that list
+    *   return the sum
+
+#### **Task 5**:
+
+In this task we had to create function using list comprehension to generate license plates. Here the format is KADDAADDDD where A is for Alphabet uppercase and D is for digit. random value is generated for DD and DDDD in the range 10 to 99 and 1000 to 9999 respectively. AA is also generated randomly using Ascii.uppercase characters.
+Then the list is returned
+
+#### **Task 6**:
+
+* In this task we can switch between KA/DL as the statecode , here the user can give a list of numbers which they want in their number plate
+* In task 2 of this we need to create a function which allows custom number plate and statecode and we need to create a partial function that calls this function providing a default number plate where as custom statecode.
+  
+
+### Test Cases
+
+Here we have written a lot of testcases to check each functionality thoroughtly as out code shouldnt be breaking. Each function has it own test case and they are also checked for doc string and annotations
+
--- Week5/test_session5.py
+++ Week5/test_session5.py
@@ -8,6 +8,12 @@ from session5 import score_calculator
 from session5 import card_deck1_regular
 from session5 import card_deck2_special
 
+import os
+import pytest
+import random
+
+
+
 ##type of Card Suits
 suits = ['spades', 'clubs', 'hearts', 'diamonds']
 ##Face value of cards
@@ -56,7 +62,19 @@ README_CONTENT_CHECK_FOR = [
     'game_card_poker_winner',
     'score_calculator',
     'card_deck1_regular',
-    'card_deck2_special'
+    'card_deck2_special',
+    'Fibonacci',
+    'vowels',
+    'list',
+    'lambda',
+    'zip',
+    'tuple',
+    'detox',
+    'reduce',
+    'sigmoid',
+    'Cipher',
+    'number plate',
+    'ReLU'
 ]
 
 def test_readme_exists():
@@ -69,34 +87,11 @@ def test_readme_contents():
     assert len(readme_words) >= 500, "Make your README.md file interesting! Add atleast 500 words"
 
 
-def test_readme_proper_description():
-    READMELOOKSGOOD = True
-    f = open("README.md", "r")
-    content = f.read()
-    f.close()
-    for c in README_CONTENT_CHECK_FOR:
-        if c not in content:
-            READMELOOKSGOOD = False
-            pass
-    assert READMELOOKSGOOD == True, "You have not described all the functions/class well in your README.md file"
-
-def test_indentations():
-    ''' Returns pass if used four spaces for each level of syntactically \
-    significant indenting.'''
-    lines = inspect.getsource(session5)
-    spaces = re.findall('\n +.', lines)
-    for space in spaces:
-        assert len(space) % 4 == 2, "Your script contains misplaced indentations"
-        assert len(re.sub(r'[^ ]', '', space)) % 4 == 0, "Your code indentation does not follow PEP8 guidelines"
-
 
 def test_function_name_had_cap_letter():
     functions = inspect.getmembers(session5, inspect.isfunction)
     for function in functions:
         assert len(re.findall('([A-Z])', function[0])) == 0, "You have used Capital letter(s) in your function names"
-
-
-
 
 def test_function_poker_game():
     assert game_card_poker_winner(p_royal_flush_5,p_straight_flush_5) =='Player 1', "Test01-High Hand Cannot lose to straigh flush"
@@ -208,3 +203,259 @@ def test_function_score_cal_check25():
 def test_function_score_cal_check26():
     assert score_calculator(p_high_card_3)[0]==1000, 'score Should be 1000'
 
+
+# Test for Assignment 2
+
+def test_add():
+    a = [1, 3, 5, 7, 9]
+    b = [2, 4, 6, 8, 10]
+    assert session5.my_add(a, b) == [], 'you just had to check odd and even and perform addition, and you FAILED..!!'
+    b = [1, 3, 5, 7, 9]
+    a = [2, 4, 6, 8, 10]
+    assert session5.my_add(a, b) == [3, 7, 11, 15,
+                                     19], 'you just had to check odd and even and perform addition, and you FAILED..!!'
+
+
+def test_string():
+    mystring = "tsai"
+    assert session5.my_alphabet(mystring) == "ts", 'You ust need to remove a,e,i, o, u, You are awesome, you can do it.'
+
+
+def test_my_relu():
+    mylist = [-1, -2, -3, -4, -5, 5, 4, 3, 1, 0, 9, -10, -17, 5, 2]
+    assert session5.my_relu(mylist) == [0, 0, 0, 0, 0, 5, 4, 3, 1, 0, 9, 0, 0, 5, 2], "Remove all the negativity"
+
+
+def test_sigmoid():
+    x = 0
+    assert session5.sigmoid(x) == 0.5, 'it is simple sigmoid, check google for equation'
+    x = 4
+    assert session5.sigmoid(x) == 0.9820137900379085, 'it is simple sigmoid, check google for equation'
+    x = -4
+    assert session5.sigmoid(x) == 0.01798620996209156, 'it is simple sigmoid, check google for equation'
+
+
+def test_mysigmoid():
+    mylist = [0, 4, -4]
+    assert session5.mysigmoid(mylist) == [0.5, 0.9820137900379085,
+                                          0.01798620996209156], 'do the same thing that you did above, but it is a list this time'
+
+
+def test_mycipher():
+    mystring = "zyxwvutsai"
+    assert session5.mycipher(
+        mystring) == "edcbazyxfn", "Alphabetical ciphers are very simple,you messed it up but you can do it"
+
+
+def test_detox():
+    mypara = "ola Amigo"
+    with pytest.raises(ValueError, match=r".* length .*"):
+        session5.detox(mypara), "That is not a paragraph"
+
+    mypara = '''
+            reverting for no reason i spent quite some time improving an article that 
+            was in a poor state now two people have undone all my work with a click 
+            of a button you did not have the courtesy to explain why you reverted it 
+            but you still had the fucking cheek to accuse me of leaving an inaccurate 
+            edit summary noone is that stupid so you were plainly just out to provoke me 
+            well fucking well done consider me fucking provoked i guess you did not even bother 
+            to look at the changes before you put all that shit back selfarrest refers to various maneuvers 
+            employed in mountaineering it does not refer to that it is that you want to say paris refers to the capital 
+            city of france or nile refers to a river that flows into the mediterranean sliding down a snow or ice covered slope 
+            arrests stops the slide you think readers are too stupid to understand what arrests means in this 
+            context himself or herself you think the single word themselves is some how better expressed in three words andor 
+            ice axe you never bothered to read wpslash these potentially lifesaving techniques must be practiced 
+            frequently in order to maintain proficiency this website is called wikipedia not wikimanual but you never bothered 
+            to read wpnot did you that is just the first five i made many more i left the article looking considerably 
+            better and more encyclopaedic but then you came along and fucked it all back up again do you feel 
+            proud still waiting for you to justify your risible claim of or also stop stalking and harassing
+            '''
+    assert session5.detox(
+        mypara) == True, "One should not ignore bad words, they should teach people not to say it as it may hurt someone"
+
+    mypara = '''
+            reverting for no reason i spent quite some time improving an article that 
+            was in a poor state now two people have undone all my work with a click 
+            of a button you did not have the courtesy to explain why you reverted it 
+            but you still had the cheek to accuse me of leaving an inaccurate 
+            edit summary noone is that stupid so you were plainly just out to provoke me 
+            well well done consider me provoked i guess you did not even bother 
+            to look at the changes before you put all that back selfarrest refers to various maneuvers 
+            employed in mountaineering it does not refer to that it is that you want to say paris refers to the capital 
+            city of france or nile refers to a river that flows into the mediterranean sliding down a snow or ice covered slope 
+            arrests stops the slide you think readers are too stupid to understand what arrests means in this 
+            context himself or herself you think the single word themselves is some how better expressed in three words andor 
+            ice axe you never bothered to read wpslash these potentially lifesaving techniques must be practiced 
+            frequently in order to maintain proficiency this website is called wikipedia not wikimanual but you never bothered 
+            to read wpnot did you that is just the first five i made many more i left the article looking considerably 
+            better and more encyclopaedic but then you came along and it all back up again do you feel 
+            proud still waiting for you to justify your risible claim of or also stop stalking and harassing
+            '''
+    assert session5.detox(mypara) == False, "One should not classify something good as bad"
+
+
+def test_myadd():
+    mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert session5.myadd(mynumbers) == 2 + 4 + 6 + 8 + 10, 'just check if they are even then add them. Simple.'
+
+
+def test_mymaxchar():
+    mystring = "SHUBHAM AGNIHOTRI"
+    assert session5.mymaxchar(mystring) == 'U', 'Need to find the max character, it is not working as expected'
+    mystring = "shubham agnihotri"
+    assert session5.mymaxchar(mystring) == 'u', 'Need to find the max character, it is not working as expected'
+
+
+def test_my_atrangi_addition():
+    mynumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    assert session5.my_atrangi_addition(mynumbers) == 3 + 6 + 9, 'just select evert 3rd element and add it. Simple.'
+
+
+def test_get_numberplate():
+    mynumbers = [random.randint(1000, 10000) for x in range(25)]
+    mynumberplates = session5.get_numberplate(mynumbers)
+    for i in range(len(mynumberplates)):
+        assert mynumberplates[i][-4:] == str(mynumbers[i]), 'Custom Number plates not getting assigned'
+
+
+def test_ny_number_plate():
+    state_code = "KA"
+    numberplate = 1224
+    assert session5.my_number_plate(state_code, numberplate)[
+           :2] == state_code, 'Number plate is not getting generated properly'
+    assert session5.my_number_plate(state_code, numberplate)[-4:] == str(
+        numberplate), 'Number plate is not getting generated properly'
+
+
+def test_custom_numberplate():
+    state_code = "KA"
+    assert session5.custom_numberplate(session5.my_number_plate, state_code)()[
+           :2] == state_code, 'Number plate is not getting generated properly'
+
+
+def test_fibonacci_doc():
+    assert bool(session5.fibonacci.__doc__), 'No DocString for fibonacci'
+
+
+def test_check_result_docs():
+    assert bool(session5.check_result.__doc__), 'No DocString for check_result'
+
+
+def test_my_add_docs():
+    assert bool(session5.my_add.__doc__), 'No DocString for my_add'
+
+
+def test_my_alphabet_docs():
+    assert bool(session5.my_alphabet.__doc__), 'No DocString for my_alphabet'
+
+
+def test_my_relu_docs():
+    assert bool(session5.my_relu.__doc__), 'No DocString for my_relu'
+
+
+def test_sigmoid_docs():
+    assert bool(session5.sigmoid.__doc__), 'No DocString for sigmoid'
+
+
+def test_mysigmoid_docs():
+    assert bool(session5.mysigmoid.__doc__), 'No DocString for mysigmoid'
+
+
+def test_mycipher_docs():
+    assert bool(session5.mycipher.__doc__), 'No DocString for mycipher'
+
+
+def test_detox_docs():
+    assert bool(session5.detox.__doc__), 'No DocString for detox'
+
+
+def test_myadd_docs():
+    assert bool(session5.myadd.__doc__), 'No DocString for myadd'
+
+
+def test_mymaxchar_docs():
+    assert bool(session5.mymaxchar.__doc__), 'No DocString for mymaxchar'
+
+
+def test_my_atrangi_addition_docs():
+    assert bool(session5.my_atrangi_addition.__doc__), 'No DocString for my_atrangi_addition'
+
+
+def test_mynumberplate_docs():
+    assert bool(session5.mynumberplate.__doc__), 'No DocString for mynumberplate'
+
+
+def test_get_numberplate_docs():
+    assert bool(session5.get_numberplate.__doc__), 'No DocString for get_numberplate'
+
+
+def test_my_number_plate_docs():
+    assert bool(session5.my_number_plate.__doc__), 'No DocString for my_number_plate'
+
+
+def test_custom_numberplate_docs():
+    assert bool(session5.custom_numberplate.__doc__), 'No DocString for custom_numberplate'
+
+
+def test_fibonacci_annot():
+    assert bool(session5.fibonacci.__annotations__), 'No Annotation for fibonacci'
+
+
+def test_check_result_annot():
+    assert bool(session5.check_result.__annotations__), 'No Annotation for check_result'
+
+
+def test_my_add_annot():
+    assert bool(session5.my_add.__annotations__), 'No Annotation for my_add'
+
+
+def test_my_alphabet_annot():
+    assert bool(session5.my_alphabet.__annotations__), 'No Annotation for my_alphabet'
+
+
+def test_my_relu_annot():
+    assert bool(session5.my_relu.__annotations__), 'No Annotation for my_relu'
+
+
+def test_sigmoid_annot():
+    assert bool(session5.sigmoid.__annotations__), 'No Annotation for sigmoid'
+
+
+def test_mysigmoid_annot():
+    assert bool(session5.mysigmoid.__annotations__), 'No Annotation for mysigmoid'
+
+
+def test_mycipher_annot():
+    assert bool(session5.mycipher.__annotations__), 'No Annotation for mycipher'
+
+
+def test_detox_annot():
+    assert bool(session5.detox.__annotations__), 'No Annotation for detox'
+
+
+def test_myadd_annot():
+    assert bool(session5.myadd.__annotations__), 'No Annotation for myadd'
+
+
+def test_mymaxchar_annot():
+    assert bool(session5.mymaxchar.__annotations__), 'No Annotation for mymaxchar'
+
+
+def test_my_atrangi_addition_annot():
+    assert bool(session5.my_atrangi_addition.__annotations__), 'No Annotation for my_atrangi_addition'
+
+
+def test_mynumberplate_annot():
+    assert bool(session5.mynumberplate.__annotations__), 'No Annotation for mynumberplate'
+
+
+def test_get_numberplate_annot():
+    assert bool(session5.get_numberplate.__annotations__), 'No Annotation for get_numberplate'
+
+
+def test_my_number_plate_annot():
+    assert bool(session5.my_number_plate.__annotations__), 'No Annotation for my_number_plate'
+
+
+def test_custom_numberplate_annot():
+    assert bool(session5.custom_numberplate.__annotations__), 'No Annotation for custom_numberplate'
